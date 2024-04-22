7b79fd0ee37fd576bc2d84fb04975e7d05207b66
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-04-22 10:49:50 -0400

    update

--- meta_experiment.sh
+++ meta_experiment.sh
@@ -5,7 +5,7 @@ eval_types=( 'clean' 'PGD' 'AA')
 for eval_type in "${eval_types[@]}"; do
 
     echo 'eval_type' $eval_type 
-    sbatch --export=ALL,EVAL_TYPE=$eval_type ./benchmark_launch.sh     
+    sbatch --export=ALL,EVAL_TYPE=$eval_type ./experiment.sh     
 
     done
     
