d1e4e50b580242ac996fd426ea52e214dc8fdd57
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-04-25 11:14:41 -0400

    update

--- experiment.sh
+++ experiment.sh
@@ -7,7 +7,7 @@
 #SBATCH --gpus-per-node=1
 
 #SBATCH --mem-per-cpu=4000M
-#SBATCH --time=05:00:00
+#SBATCH --time=00:30:00
 
 #SBATCH --mail-user=maxime.heuillet.1@ulaval.ca
 #SBATCH --mail-type=ALL
