fc3874887a220e93cac9e10d1e4734b843e48bb4
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-04-26 17:46:10 -0400

    update

--- active.py
+++ active.py
@@ -100,7 +100,7 @@ def random_sampling(model, loader, n_instances=10):
 
 def attack_sampling(model, loader):
     device = 'cuda'
-    model.eval()
+    
     all_indices = []
     successful_attack_indices = []
     epsilon = 8/255
@@ -108,23 +108,24 @@ def attack_sampling(model, loader):
     attack_iters = 50
     alpha = epsilon/4
     
-    with torch.no_grad():
-        for i, (images, labels) in enumerate(loader):
-            images = images.to(device)
-            labels = labels.to(device)
-            # Apply the attack to the images
+    for i, (images, labels) in enumerate(loader):
+        model.trail()
+        images = images.to(device)
+        labels = labels.to(device)
+        # Apply the attack to the images
 
-            delta = utils.attack_pgd_restart(model, images, labels, epsilon, alpha, attack_iters, n_restarts, rs=True, verbose=False,
+        delta = utils.attack_pgd_restart(model, images, labels, epsilon, alpha, attack_iters, n_restarts, rs=True, verbose=False,
                linf_proj=True, l2_proj=False, l2_grad_update=False, cuda=True)
         
-            x_adv = images + delta
+        x_adv = images + delta
 
-            outputs = model( x_adv )
-            _, predicted_labels = torch.max(outputs, dim=1)
-            # Compare predictions with the true labels
-            attack_success = predicted_labels != labels
-            # Store indices of successfully attacked observations
-            batch_indices = [idx for idx, success in zip(range(i * loader.batch_size, i * loader.batch_size + len(labels)), attack_success) if success]
-            successful_attack_indices.extend(batch_indices)
+        model.eval()
+        outputs = model( x_adv )
+        _, predicted_labels = torch.max(outputs, dim=1)
+        # Compare predictions with the true labels
+        attack_success = predicted_labels != labels
+        # Store indices of successfully attacked observations
+        batch_indices = [idx for idx, success in zip(range(i * loader.batch_size, i * loader.batch_size + len(labels)), attack_success) if success]
+        successful_attack_indices.extend(batch_indices)
     
     return successful_attack_indices
