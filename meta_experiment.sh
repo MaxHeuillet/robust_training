7c3e7b1332ab696afe36f521167416d5463f83a8
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-04-29 15:49:04 -0400

    update

--- meta_experiment.sh
+++ meta_experiment.sh
@@ -13,19 +13,19 @@ for ((id=0; id<$seeds; id+=1)); do
 
     sbatch --export=ALL,NROUNDS=6,NBEPOCHS=10,RSIZE=0,ASTRAT='full',MODEL=$model,DATA=$data,SEED=$id ./experiment.sh
 
-    sbatch --export=ALL,NROUNDS=6,NBEPOCHS=10,RSIZE=1000,ASTRAT='random',SEED=$id ./experiment.sh
-    sbatch --export=ALL,NROUNDS=6,NBEPOCHS=10,RSIZE=1000,ASTRAT='uncertainty',SEED=$id ./experiment.sh
-    sbatch --export=ALL,NROUNDS=6,NBEPOCHS=10,RSIZE=1000,ASTRAT='margin',SEED=$id ./experiment.sh
-    sbatch --export=ALL,NROUNDS=6,NBEPOCHS=10,RSIZE=1000,ASTRAT='entropy',SEED=$id ./experiment.sh
-    sbatch --export=ALL,NROUNDS=6,NBEPOCHS=10,RSIZE=1000,ASTRAT='attack',SEED=$id ./experiment.sh
+    # sbatch --export=ALL,NROUNDS=6,NBEPOCHS=10,RSIZE=1000,ASTRAT='random',SEED=$id ./experiment.sh
+    # sbatch --export=ALL,NROUNDS=6,NBEPOCHS=10,RSIZE=1000,ASTRAT='uncertainty',SEED=$id ./experiment.sh
+    # sbatch --export=ALL,NROUNDS=6,NBEPOCHS=10,RSIZE=1000,ASTRAT='margin',SEED=$id ./experiment.sh
+    # sbatch --export=ALL,NROUNDS=6,NBEPOCHS=10,RSIZE=1000,ASTRAT='entropy',SEED=$id ./experiment.sh
+    # sbatch --export=ALL,NROUNDS=6,NBEPOCHS=10,RSIZE=1000,ASTRAT='attack',SEED=$id ./experiment.sh
 
     sbatch --export=ALL,NROUNDS=1,NBEPOCHS=60,RSIZE=0,ASTRAT='full',MODEL=$model,DATA=$data,SEED=$id ./experiment.sh
 
-    sbatch --export=ALL,NROUNDS=1,NBEPOCHS=60,RSIZE=6000,ASTRAT='random',SEED=$id ./experiment.sh
-    sbatch --export=ALL,NROUNDS=1,NBEPOCHS=60,RSIZE=6000,ASTRAT='uncertainty',SEED=$id ./experiment.sh
-    sbatch --export=ALL,NROUNDS=1,NBEPOCHS=60,RSIZE=6000,ASTRAT='margin',SEED=$id ./experiment.sh
-    sbatch --export=ALL,NROUNDS=1,NBEPOCHS=60,RSIZE=6000,ASTRAT='entropy',SEED=$id ./experiment.sh
-    sbatch --export=ALL,NROUNDS=1,NBEPOCHS=60,RSIZE=6000,ASTRAT='attack',SEED=$id ./experiment.sh
+    # sbatch --export=ALL,NROUNDS=1,NBEPOCHS=60,RSIZE=6000,ASTRAT='random',SEED=$id ./experiment.sh
+    # sbatch --export=ALL,NROUNDS=1,NBEPOCHS=60,RSIZE=6000,ASTRAT='uncertainty',SEED=$id ./experiment.sh
+    # sbatch --export=ALL,NROUNDS=1,NBEPOCHS=60,RSIZE=6000,ASTRAT='margin',SEED=$id ./experiment.sh
+    # sbatch --export=ALL,NROUNDS=1,NBEPOCHS=60,RSIZE=6000,ASTRAT='entropy',SEED=$id ./experiment.sh
+    # sbatch --export=ALL,NROUNDS=1,NBEPOCHS=60,RSIZE=6000,ASTRAT='attack',SEED=$id ./experiment.sh
 
     done
 
