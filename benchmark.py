39540c3cf5ba0bc63264114c38b9016cbdf8a8af
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-04-29 15:47:07 -0400

    update

--- benchmark.py
+++ benchmark.py
@@ -23,6 +23,9 @@ import active
 import torch.optim as optim
 from torch import nn
 
+import pickle as pkl
+import gzip
+
 ###################################
 # Experiments
 ###################################
@@ -38,18 +41,37 @@ parser.add_argument("--n_rounds", required=True, help="nb of active learning rou
 parser.add_argument("--nb_epochs", required=True, help="nb of Lora epochs")
 parser.add_argument("--round_size", required=True, help="wether to compute clean accuracy, PGD robustness or AA robustness")
 parser.add_argument("--active_strategy", required=True, help="which observation strategy to choose")
+parser.add_argument("--seed", required=True, help="the random seed")
+parser.add_argument("--data", required=True, help="the data used for the experiment")
 
 args = parser.parse_args()
+
+
+result = {}
 
 n_rounds = int(args.n_rounds)
 round_size = int(args.round_size)
 nb_epochs = int(args.nb_epochs)
+seed = int(args.seed)
+
+result['n_rounds'] = n_rounds
+result['round_size'] = round_size
+result['nb_epochs'] = nb_epochs
+result['seed'] = seed
+result['data'] = args.data
+result['model'] = args.model
 
 ############################# INITIATE THE EXPERIMENT:
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = models.resnet.resnet50(pretrained=True, progress=True, device="cuda").to('cuda')
+utils.set_seeds(seed)
+
+if args.model == 'resnet50':
+    model = models.resnet.resnet50(pretrained=True, progress=True, device="cuda").to('cuda')
+else:
+    print('model undefined')
+
 print('model loaded successfully')
 sys.stdout.flush()
 
@@ -58,23 +80,32 @@ sys.stdout.flush()
 print('begin the evaluation experiment with {} // {} // {} // {} '.format(args.active_strategy, args.n_rounds,args.round_size,args.nb_epochs) )
 sys.stdout.flush()
 
-mean_cifar10 = (0.4914, 0.4822, 0.4465)
-std_cifar10 = (0.2471, 0.2435, 0.2616)
+if args.data == 'CIFAR10':
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(mean_cifar10, std_cifar10)  ])
+    mean_cifar10 = (0.4914, 0.4822, 0.4465)
+    std_cifar10 = (0.2471, 0.2435, 0.2616)
 
-test_dataset = datasets.CIFAR10(root='./data', train=False, transform=transform)
-test_loader = DataLoader(test_dataset, batch_size=1000, shuffle=False)
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean_cifar10, std_cifar10)  ])
+
+    test_dataset = datasets.CIFAR10(root='./data', train=False, transform=transform)
+    test_loader = DataLoader(test_dataset, batch_size=1000, shuffle=False)
+
+else:
+    print('undefined data')
 
 accuracy = utils.compute_clean_accuracy(model, test_loader)
 print(f'The clean accuracy of the model on the test set is {accuracy}%')
 sys.stdout.flush()
 
+result['init_clean_accuracy'] = accuracy
+
 accuracy = utils.compute_PGD_accuracy(model, test_loader, device='cuda')
 print(f'The PGD accuracy of the model on the test set is {accuracy}%')
 sys.stdout.flush()
+
+result['init_PGD_accuracy'] = accuracy
 
 ##############################################
 
@@ -125,42 +156,32 @@ for i in range(n_rounds):
 
     print( 'iteration {}'.format(i) )
 
-    if args.active_strategy != 'full':
-    
-        pool_loader = DataLoader( Subset(pool_dataset, pool_indices), batch_size=1000, shuffle=False)
+    pool_loader = DataLoader( Subset(pool_dataset, pool_indices), batch_size=1000, shuffle=False)
         
-        if args.active_strategy == 'uncertainty':
-            query_indices = active.uncertainty_sampling(model, pool_loader, round_size)
-        elif args.active_strategy == 'entropy':
-            query_indices = active.entropy_sampling(model, pool_loader, round_size)
-        elif args.active_strategy == 'margin':
-            query_indices = active.margin_sampling(model, pool_loader, round_size)
-        elif args.active_strategy == 'random':
-            query_indices = active.random_sampling(model, pool_loader, round_size)
-        elif args.active_strategy == 'attack':
-            query_indices = active.attack_sampling(model, pool_loader, round_size)
-        else:
-            print('error')
-
-        global_query_indices = [ pool_indices[idx] for idx in query_indices]
-
-        for idx in global_query_indices:
-            image, label = pool_dataset[idx]
-            adapt_dataset.add_data( [image], [label] )
-
-        # Remove queried instances from the pool
-        pool_indices = [idx for idx in pool_indices if idx not in global_query_indices]
-        print( len(pool_indices) )
-
-        adapt_loader = DataLoader(adapt_dataset, batch_size=128, shuffle=True)
-
+    if args.active_strategy == 'uncertainty':
+        query_indices = active.uncertainty_sampling(model, pool_loader, round_size)
+        pool_indices = utils.add_data(query_indices, pool_indices, pool_dataset, adapt_dataset)
+    elif args.active_strategy == 'entropy':
+        query_indices = active.entropy_sampling(model, pool_loader, round_size)
+        pool_indices = utils.add_data(query_indices, pool_indices, pool_dataset, adapt_dataset)
+    elif args.active_strategy == 'margin':
+        query_indices = active.margin_sampling(model, pool_loader, round_size)
+        pool_indices = utils.add_data(query_indices, pool_indices, pool_dataset, adapt_dataset)
+    elif args.active_strategy == 'random':
+        query_indices = active.random_sampling(model, pool_loader, round_size)
+        pool_indices = utils.add_data(query_indices, pool_indices, pool_dataset, adapt_dataset)
+    elif args.active_strategy == 'attack':
+        query_indices = active.attack_sampling(model, pool_loader, round_size)
+        pool_indices = utils.add_data(query_indices, pool_indices, pool_dataset, adapt_dataset)
     elif args.active_strategy == 'full':
-
-        adapt_loader = DataLoader(pool_dataset, batch_size=128, shuffle=True)
-
+        query_indices = utils.get_indices_for_round( len(pool_loader.dataset), n_rounds, i)
+        pool_indices = utils.add_data(query_indices, pool_indices, pool_dataset, adapt_dataset)
     else:
         print('error')
-    
+
+    print( len(query_indices) )
+    adapt_loader = DataLoader(adapt_dataset, batch_size=128, shuffle=True)
+    print( len(adapt_loader.dataset) )
 
     ################# Update the ResNet through low rank matrices:
     print('start training process')
@@ -207,16 +228,23 @@ sys.stdout.flush()
 print('begin the evaluation experiment with {} // {} // {} // {} '.format(args.active_strategy, args.n_rounds,args.round_size,args.nb_epochs) )
 sys.stdout.flush()
 
-test_dataset = datasets.CIFAR10(root='./data', train=False, transform=transform)
-test_loader = DataLoader(test_dataset, batch_size=1000, shuffle=False)
-
 accuracy = utils.compute_clean_accuracy(model, test_loader)
 print(f'The clean accuracy of the model on the test set is {accuracy}%')
 sys.stdout.flush()
 
+result['final_clean_accuracy'] = accuracy
+
 accuracy = utils.compute_PGD_accuracy(model, test_loader, device='cuda')
 print(f'The PGD accuracy of the model on the test set is {accuracy}%')
 sys.stdout.flush()
+
+result['final_PGD_accuracy'] = accuracy
+
+print(result)
+print('finished')
+with gzip.open( './results/{}_{}_{}_{}_{}_{}.pkl.gz'.format(args.data, args.model, n_rounds, round_size, nb_epochs, seed) ,'ab') as f:
+        pkl.dump(result,f)
+print('saved')
 
 #if args.eval_type == 'clean':
 #elif args.eval_type == 'PGD':
