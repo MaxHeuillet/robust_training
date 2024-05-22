8eb8a6b8620a106329969cd2b8f83fe1445c2c1f
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-05-22 10:48:42 -0400

    update

--- load_model.py
+++ load_model.py
@@ -4,3 +4,8 @@ import torch
 model = ResNetModel.from_pretrained("microsoft/resnet-50")
 
 model.save_pretrained("/home/mheuill/scratch/resnet-50")
+
+
+from transformers import AutoImageProcessor
+image_processor = AutoImageProcessor.from_pretrained("microsoft/resnet-50")
+image_processor.save_pretrained("/home/mheuill/scratch/resnet-50")
