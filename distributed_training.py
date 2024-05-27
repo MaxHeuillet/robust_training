9ba69f6d223b760584cfa63a455436aeff7c1e19
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-05-27 10:44:14 -0400

    update

--- distributed_training.py
+++ distributed_training.py
@@ -50,27 +50,25 @@ class CustomImageDataset(Dataset):
 
 
 
-#def setup(rank, world_size):
-# Initialize the distributed environment.
+def setup(rank, world_size):
+  #Initialize the distributed environment.
+  print('set up the master adress and port')
+  os.environ['MASTER_ADDR'] = 'localhost'
+  os.environ['MASTER_PORT'] = '12355'
+  #Set environment variables for offline usage of Hugging Face libraries
+  os.environ['HF_DATASETS_OFFLINE'] = '1'
+  os.environ['TRANSFORMERS_OFFLINE'] = '1'
+  
+  #Set up the local GPU for this process
+  torch.cuda.set_device(rank)
+  dist.init_process_group("nccl", rank=rank, world_size=world_size)
+  print('init process group ok')
 
-#    print('set up the master adress and port')
-#   os.environ['MASTER_ADDR'] = 'localhost'
-#   os.environ['MASTER_PORT'] = '12355'
-
-# Set environment variables for offline usage of Hugging Face libraries
-#    os.environ['HF_DATASETS_OFFLINE'] = '1'
-#    os.environ['TRANSFORMERS_OFFLINE'] = '1'
-
-# Set up the local GPU for this process
-#    torch.cuda.set_device(rank)
-#    dist.init_process_group("nccl", rank=rank, world_size=world_size)
-#    print('init process group ok')
-
-#def cleanup():
-#    dist.destroy_process_group()
+def cleanup():
+   dist.destroy_process_group()
 
 
-def inference():
+def inference(world_size, rank):
     #setup(rank, world_size)
 
     print('load dataset')
@@ -81,11 +79,11 @@ def inference():
     # dataset = image_processor(dataset)
 
     print('create custom dataset')
-    print(dataset['train'][0]['image'])
+    # print(dataset['train'][0]['image'])
 
     # Define your transformations
     transform = transforms.Compose([
-        transforms.Lambda(lambda x: x.convert("RGB")),
+        transforms.Lambda(lambda x: x.convert("RGB") ),
         transforms.Resize(256),  # Resize so the shortest side is 256 pixels
         transforms.CenterCrop(224),  # Crop the center 224x224 pixels
         transforms.ToTensor(),  # Convert image to tensor
@@ -95,15 +93,15 @@ def inference():
     # Instantiate the custom dataset
     dataset = CustomImageDataset(dataset['test'], transform=transform)
 
-    #sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank, shuffle=False)
+    sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank, shuffle=False)
 
     print('load dataloader')
-    dataloader = DataLoader(dataset, batch_size=1024 )
+    dataloader = DataLoader(dataset, batch_size=1024, sampler=sampler, num_workers=4)
 
     # Load model
     model = ResNetModel.from_pretrained('/home/mheuill/scratch/resnet-50', local_files_only=True) #resnet50().cuda(rank)
-    model = model.cuda()
-    #model = DDP(model, device_ids=[rank])
+    model = model.cuda(rank)
+    model = DDP(model, device_ids=[rank])
     model.eval()
 
     predictions = []
@@ -111,78 +109,75 @@ def inference():
     print('start the loop')
     with torch.no_grad():
         for inputs, _ in dataloader:
-            inputs = inputs.cuda()
-            outputs = model(inputs)
-            # predictions.append(outputs)
+            inputs = inputs.cuda(rank)
+            outputs = model(inputs).last_hidden_state
+            predictions.append(outputs)
 
             time +=1
             
-            if time > 10:
-                print(inputs.shape, dir(outputs)  )
-                print( "time {time} " )
-                break
-
     # Gather all predictions to the process 0
-    #predictions = torch.cat(predictions, dim=0)
-    #gather_list = [torch.zeros_like(predictions) for _ in range(world_size)]
-    #dist.all_gather(gather_list, predictions)
+    predictions = torch.cat(predictions, dim=0)
+    gather_list = [torch.zeros_like(predictions) for _ in range(world_size)]
+    dist.all_gather(gather_list, predictions)
 
-    #if rank == 0:
+    if rank == 0:
         # Concatenate all predictions on rank 0
-    #    all_predictions = torch.cat(gather_list, dim=0)
-    #   print(all_predictions.shape)  # This will show the total number of predictions
-
-    #cleanup()
-
-def train(rank, world_size):
-
-
-    setup(rank, world_size)
-
-    print('load dataset')
-    dataset = load_dataset("imagenet-1k", cache_dir='/home/mheuill/scratch')
-    dataset = dataset['train'] 
-
-    print(dataset['train'][0]['image'])
-
-    print('load sampler')
-    #sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank)
-
-    print('load dataloader')
-    dataloader = DataLoader(dataset, batch_size=1024, num_workers=1)
-
-    # Model, Loss, and Optimizer
-    model = resnet50().cuda()
-    #model = DDP(model, device_ids=[rank])
-    print('load model')
-
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-    # Training Loop
-    for epoch in range(1):  # number of epochs
-
-        print( 'epoch {}'.format(epoch) )
-        sampler.set_epoch(epoch)
-
-        for i, (inputs, targets) in enumerate(dataloader):
-            inputs = inputs.cuda()
-            targets = targets.cuda()
-
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, targets)
-            loss.backward()
-            optimizer.step()
-
-            if i % 10 == 0:
-                print(f"Rank {rank}, Epoch {epoch}, Batch {i}, Loss {loss.item()}")
+        all_predictions = torch.cat(gather_list, dim=0)
+        print(all_predictions.shape)  # This will show the total number of predictions
 
     cleanup()
 
-#if __name__ == "__main__":
-print('begin experiment')
-inference()
-#world_size = 1   
-#torch.multiprocessing.spawn(inference, args=(world_size,), nprocs=world_size)
-# torch.multiprocessing.spawn(train, args=(world_size,), nprocs=world_size)
+if __name__ == "__main__":
+    print('begin experiment')
+    world_size = 4  
+    torch.multiprocessing.spawn(inference, args=(world_size,), nprocs=world_size)
+    
+    
+    
+    
+    
+    
+    
+    
+#torch.multiprocessing.spawn(train, args=(world_size,), nprocs=world_size)
+# Gather all predictions to the process 0
+#predictions = torch.cat(predictions, dim=0)
+#gather_list = [torch.zeros_like(predictions) for _ in range(world_size)]
+#dist.all_gather(gather_list, predictions)
+#if rank == 0:
+# Concatenate all predictions on rank 0
+#    all_predictions = torch.cat(gather_list, dim=0)
+#   print(all_predictions.shape)  # This will show the total number of predictions
+#cleanup()
+
+# def train(rank, world_size):
+#     setup(rank, world_size)
+#     print('load dataset')
+#     dataset = load_dataset("imagenet-1k", cache_dir='/home/mheuill/scratch')
+#     dataset = dataset['train'] 
+#     print(dataset['train'][0]['image'])
+#     print('load sampler')
+#     #sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank)
+#     print('load dataloader')
+#     dataloader = DataLoader(dataset, batch_size=1024, num_workers=1)
+#     # Model, Loss, and Optimizer
+#     model = resnet50().cuda()
+#     #model = DDP(model, device_ids=[rank])
+#     print('load model')
+#     criterion = nn.CrossEntropyLoss()
+#     optimizer = optim.Adam(model.parameters(), lr=0.001)
+#     # Training Loop
+#     for epoch in range(1):  # number of epochs
+#         print( 'epoch {}'.format(epoch) )
+#         sampler.set_epoch(epoch)
+#         for i, (inputs, targets) in enumerate(dataloader):
+#             inputs = inputs.cuda()
+#             targets = targets.cuda()
+#             optimizer.zero_grad()
+#             outputs = model(inputs)
+#             loss = criterion(outputs, targets)
+#             loss.backward()
+#             optimizer.step()
+#             if i % 10 == 0:
+#                 print(f"Rank {rank}, Epoch {epoch}, Batch {i}, Loss {loss.item()}")
+#     cleanup()
