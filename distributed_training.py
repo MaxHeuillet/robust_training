708bdf67bef1f4784751e01fb99c4354f578e76b
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-06-07 13:41:04 -0400

    update

--- distributed_training.py
+++ distributed_training.py
@@ -257,6 +257,24 @@ class Experiment:
             test_dataset = CustomImageDataset(dataset['test'], transform=transform)
 
             print('load dataloader')
+
+        elif self.data == 'Imagenette':
+
+            transform = transforms.Compose([
+                transforms.Lambda(to_rgb),
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ])
+
+            dataset = load_dataset("imagenet-1k", cache_dir='/home/mheuill/scratch',)
+
+            pool_dataset = CustomImageDataset(dataset['test'], transform=transform)
+        
+            test_dataset = CustomImageDataset(dataset['test'], transform=transform)
+
+            print('load dataloader')
             
         else:
             print('undefined data')
