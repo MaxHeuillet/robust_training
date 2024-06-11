62eef6e9a0127b5434a8c8394b585a3fce5337c5
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-06-11 14:37:18 -0400

    update

--- distributed_training.py
+++ distributed_training.py
@@ -420,7 +420,7 @@ if __name__ == "__main__":
     n_rounds = 1
     size = 100
     nb_epochs = 1
-    seed = 2
+    seed = 3
     active_strategy = 'uncertainty'
     data = 'Imagenette'
     model = 'resnet50'
