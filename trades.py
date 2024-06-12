edf2ddb0f8766a48e99383297d047939ea7f43c5
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-06-12 17:27:21 -0400

    update

--- trades.py
+++ trades.py
@@ -108,41 +108,41 @@ def trades_loss(model,
         for _ in range(perturb_steps):
             x_adv.requires_grad_()
             with torch.enable_grad():
-                loss_kl = criterion_kl(F.log_softmax(model(x_adv), dim=1), F.softmax(model(x_natural), dim=1))
+                loss_kl = criterion_kl( F.log_softmax(model(x_adv), dim=1), F.softmax(model(x_natural), dim=1))
                 print('loss_kl',loss_kl)
             grad = torch.autograd.grad(loss_kl, [x_adv])[0]
             x_adv = x_adv.detach() + step_size * torch.sign(grad.detach())
             x_adv = torch.min(torch.max(x_adv, x_natural - epsilon), x_natural + epsilon)
             x_adv = torch.clamp(x_adv, 0.0, 1.0)
 
-    elif distance == 'l_2':
-        delta = 0.001 * torch.randn(x_natural.shape, device=x_natural.device).detach()
-        delta = Variable(delta.data, requires_grad=True)
+    # elif distance == 'l_2':
+    #     delta = 0.001 * torch.randn(x_natural.shape, device=x_natural.device).detach()
+    #     delta = Variable(delta.data, requires_grad=True)
 
-        # Setup optimizers
-        optimizer_delta = optim.SGD([delta], lr=epsilon / perturb_steps * 2)
+    #     # Setup optimizers
+    #     optimizer_delta = optim.SGD([delta], lr=epsilon / perturb_steps * 2)
 
-        for _ in range(perturb_steps):
-            adv = x_natural + delta
+    #     for _ in range(perturb_steps):
+    #         adv = x_natural + delta
 
-            # optimize
-            optimizer_delta.zero_grad()
-            with torch.enable_grad():
-                loss = (-1) * criterion_kl(F.log_softmax(model(adv), dim=1),  F.softmax(model(x_natural), dim=1))
-            loss.backward()
-            # renorming gradient
-            grad_norms = delta.grad.view(batch_size, -1).norm(p=2, dim=1)
-            delta.grad.div_(grad_norms.view(-1, 1, 1, 1))
-            # avoid nan or inf if gradient is 0
-            if (grad_norms == 0).any():
-                delta.grad[grad_norms == 0] = torch.randn_like(delta.grad[grad_norms == 0])
-            optimizer_delta.step()
+    #         # optimize
+    #         optimizer_delta.zero_grad()
+    #         with torch.enable_grad():
+    #             loss = (-1) * criterion_kl(F.log_softmax(model(adv), dim=1),  F.softmax(model(x_natural), dim=1))
+    #         loss.backward()
+    #         # renorming gradient
+    #         grad_norms = delta.grad.view(batch_size, -1).norm(p=2, dim=1)
+    #         delta.grad.div_(grad_norms.view(-1, 1, 1, 1))
+    #         # avoid nan or inf if gradient is 0
+    #         if (grad_norms == 0).any():
+    #             delta.grad[grad_norms == 0] = torch.randn_like(delta.grad[grad_norms == 0])
+    #         optimizer_delta.step()
 
-            # projection
-            delta.data.add_(x_natural)
-            delta.data.clamp_(0, 1).sub_(x_natural)
-            delta.data.renorm_(p=2, dim=0, maxnorm=epsilon)
-        x_adv = Variable(x_natural + delta, requires_grad=False)
+    #         # projection
+    #         delta.data.add_(x_natural)
+    #         delta.data.clamp_(0, 1).sub_(x_natural)
+    #         delta.data.renorm_(p=2, dim=0, maxnorm=epsilon)
+    #     x_adv = Variable(x_natural + delta, requires_grad=False)
     else:
         x_adv = torch.clamp(x_adv, 0.0, 1.0)
     model.train()
