7a788bc3f357b63c622a11d2f3150177f4adb0ef
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-06-12 10:01:53 -0400

    update

--- load_data.py
+++ load_data.py
@@ -6,16 +6,16 @@ from torchvision import datasets
 
 from datasets import load_dataset 
 
-# print('load')
-# dataset = load_dataset("frgfm/imagenette", "full_size", cache_dir='/home/mheuill/scratch')
-# print('save')
-# dataset.save_to_disk('/home/mheuill/scratch/imagenette')
-
-
 print('load')
-dataset = load_dataset('imagenet-1k',  cache_dir='/home/mheuill/scratch')
+dataset = load_dataset("frgfm/imagenette", "full_size", cache_dir='/home/mheuill/scratch')
 print('save')
-dataset.save_to_disk('/home/mheuill/scratch/imagenet-1k')
+dataset.save_to_disk('/home/mheuill/scratch/imagenette')
+
+
+# print('load')
+# dataset = load_dataset('imagenet-1k',  cache_dir='/home/mheuill/scratch')
+# print('save')
+# dataset.save_to_disk('/home/mheuill/scratch/imagenet-1k')
 
 # print('download CIFAR10')
 
