084608fa475ccb198b14eb660fbe90211f599967
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-06-12 11:42:05 -0400

    update

--- lora.py
+++ lora.py
@@ -35,6 +35,7 @@ class LoRA(nn.Module):
         self.scale = alpha / rank
 
     def forward(self, W):
+        print('test')
         return W + torch.matmul(self.mat_B, self.mat_A).view(W.shape) * self.scale
 
 def layer_parametrization(layer, device, rank = 10, lora_alpha = 1):
