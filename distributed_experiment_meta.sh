0edeabc1093d6f5574e405bbe29fbf7abb220bc2
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-06-21 18:39:01 -0400

    update

--- distributed_experiment_meta.sh
+++ distributed_experiment_meta.sh
@@ -4,6 +4,7 @@ seeds=$1
 
 model='resnet50'
 data=$2 #'Imagenet1k' #'CIFAR10'
+task=$3
 
 
 sizes=( 1 2.5 10 25 50 75 100 ) 
@@ -14,10 +15,10 @@ for size in "${sizes[@]}"; do
     for strategy in "${strategies[@]}"; do
         for ((id=0; id<$seeds; id++)); do
             if [ "$CC_CLUSTER" = "cedar" ]; then
-                sbatch --export=ALL,NROUNDS=1,NBEPOCHS=60,SIZE=$size,ASTRAT=$strategy,DATA=$data,MODEL=$model,SEED=$id ./distributed_experiment_cedar.sh
+                sbatch --export=ALL,NROUNDS=1,NBEPOCHS=60,TASK=$task,SIZE=$size,ASTRAT=$strategy,DATA=$data,MODEL=$model,SEED=$id ./distributed_experiment_cedar.sh
 
             else
-                sbatch --export=ALL,NROUNDS=1,NBEPOCHS=60,SIZE=$size,ASTRAT=$strategy,DATA=$data,MODEL=$model,SEED=$id ./distributed_experiment_other.sh
+                sbatch --export=ALL,NROUNDS=1,NBEPOCHS=60,TASK=$task,SIZE=$size,ASTRAT=$strategy,DATA=$data,MODEL=$model,SEED=$id ./distributed_experiment_other.sh
             fi
 
         
