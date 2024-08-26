e1d776c974762432f9e76db4198f309b22ed6469
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-08-26 14:54:32 -0400

    update

--- losses/loss_loader.py
+++ losses/loss_loader.py
@@ -3,10 +3,10 @@ from losses.trades import trades_loss
 from losses.trades_v2 import trades_loss_v2
 
 
-def get_loss(args, model, x_natural, y, optimizer):
+def get_loss(args, model, x_natural, y, optimizer, train):
 
     if args.loss_function == 'TRADES':
-        return trades_loss(args, model, x_natural, y, optimizer)
+        return trades_loss(args, model, x_natural, y, optimizer, train)
     elif args.loss_function == 'TRADESv2':
         return trades_loss_v2(model, x_natural, y, optimizer)
     # elif args.loss_function == 'MADRY':
