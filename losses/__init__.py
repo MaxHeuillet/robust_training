16f88ed401e24618cafb1e614c43772f454f5e35
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-08-27 22:19:53 -0400

    update

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,6 +1,6 @@
 from .trades import trades_loss
 from .trades_v2 import trades_loss_v2
-from .trades_v2 import trades_loss_v3
+from .trades_v3 import trades_loss_v3
 
 from .eval_trades import trades_loss_eval
 from .loss_loader import get_loss, get_eval_loss
