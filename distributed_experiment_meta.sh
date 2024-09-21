39c0219adc1ae8af3a6e39e217c6662a07cb2893
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-09-21 00:14:13 -0400

    update

--- distributed_experiment_meta.sh
+++ distributed_experiment_meta.sh
@@ -10,7 +10,7 @@ sched=$5  # 'sched' or 'nosched'
 iterations=$6
 
 init_lrs=( 0.2 )  # 
-pruning_ratios=( 0.7 )  #0.3 0.5  
+pruning_ratios=( 0.7 0.3 0.5 )  #  
 pruning_strategies=( 'decay_based_v2' 'TS_pruning' 'random' 'uncertainty' 'score_v1' 'score_v2' ) # 'decay_based'   'TS_context' 
 batch_strategies=('random')
 
