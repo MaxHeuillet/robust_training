3f374f9a92f24ecc3f0f63d7b3a2bf355e4fcbf5
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-09-28 22:43:10 -0400

    update

--- architectures/resnet_imagenet.py
+++ architectures/resnet_imagenet.py
@@ -94,7 +94,7 @@ class ResNet_imagenet(nn.Module):
         # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
         if zero_init_residual:
             for m in self.modules():
-                if isinstance(m, Bottleneck):
+                if isinstance(m, Bottleneck_imagenet):
                     nn.init.constant_(m.bn3.weight, 0)
 
     def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
