d078e31921070bd915e5868ed6ab06ea911f4268
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-09-28 20:48:35 -0400

    update

--- architectures/lora.py
+++ architectures/lora.py
@@ -9,7 +9,10 @@ from torch import nn
 import torch
 
 from functools import partial
+
 import torch.nn.utils.parametrize as parametrize
+
+from peft import LoraConfig, get_peft_model
 
 class LoRA(nn.Module):
     def __init__(self, features_in, features_out, rank=1, alpha=1, device='cuda'):
@@ -35,27 +38,64 @@ def layer_parametrization(layer, device, rank = 10, lora_alpha = 1):
   return LoRA(features_in, features_out, rank = rank, alpha = lora_alpha, device = device)
      
 
-def set_lora_gradients(model, layers):
+def set_lora_gradients(args, model, layers):
   # this is to freeze the main parameters of the model and put gradient tracking on the lora matrices
-  for name, param in model.named_parameters():
-    if 'mat' not in name:
-      # print(f'Freezing non-LoRA parameter {name}')
-      param.requires_grad = False
   
-  for layer in layers:
-    layer.parametrizations["weight"][0].requires_grad = True
+  if args.arch == 'ViTs':
+    for param in model.parameters():
+        param.requires_grad = False
+    # Only LoRA parameters will be trainable
+    for name, param in model.named_parameters():
+       if 'lora' in name:
+          param.requires_grad = True
   
-  for name, param in model.named_parameters():
-    if param.requires_grad:
-      pass
-      # print(f"Parameter: {name}, Shape: {param.size()}")
-
-
-
-def add_lora(target_layers, model):
-
-  for conv_layer in target_layers:
-    lora_param = layer_parametrization(conv_layer, device="cuda", rank=10, lora_alpha=1)
-    parametrize.register_parametrization(conv_layer, 'weight', lora_param)
+  elif args.arch == 'resnet50':
+    for name, param in model.named_parameters():
+      if 'mat' not in name:
+        # print(f'Freezing non-LoRA parameter {name}')
+        param.requires_grad = False
+    
+    for layer in layers:
+      layer.parametrizations["weight"][0].requires_grad = True
+    
+    for name, param in model.named_parameters():
+      if param.requires_grad:
+        pass
+        # print(f"Parameter: {name}, Shape: {param.size()}")
+  else:
+    print('LoRA is not implemented for this architecture')
   
-  set_lora_gradients(model, target_layers)
+  return model
+  
+
+
+
+def add_lora(args, model, layers):
+
+  if args.arch == 'resnet50':
+
+    for conv_layer in layers:
+      lora_param = layer_parametrization(conv_layer, device="cuda", rank=10, lora_alpha=1)
+      parametrize.register_parametrization(conv_layer, 'weight', lora_param)
+    
+  elif args.arch == 'ViTs':
+      # Define the LoRA configuration
+      lora_config = LoraConfig(
+          r=8,          # Rank of the low-rank approximation
+          lora_alpha=16, # Scaling factor for LoRA
+          lora_dropout=0.1, # Dropout for LoRA
+          target_modules=layers,  # Specify the transformer modules where LoRA should be applied
+      )
+
+      # Apply the LoRA adapters to the model
+      model = get_peft_model(model, lora_config)
+
+  else:
+    print('LoRA is not implemented for this architecture')
+    return model
+
+
+
+
+
+
