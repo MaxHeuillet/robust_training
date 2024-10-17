abcb61644ab8e0b4a78c30ffe7bc9d61e07f7583
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-10-17 15:13:06 -0400

    update

--- RQ1.sh
+++ RQ1.sh
@@ -3,7 +3,7 @@
 # Define variables
 seeds=1
 archs=( 'convnext' ) # 'resnet50' 'vitsmall'
-datas=( 'Aircraft', ) #  'Aircraft'  'EuroSAT', 'CIFAR100', 'CIFAR10', ,
+datas=( 'CIFAR100', ) #  'Aircraft'  'EuroSAT', 'CIFAR100', 'CIFAR10', ,
 task='train'
 losses=('TRADES_v2', 'APGD')
 sched='nosched'
