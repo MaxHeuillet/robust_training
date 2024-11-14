75f15d1a56c76cb3401c6faf3f7f92aa01981d2f
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-11-14 15:03:06 -0500

    update

--- losses/trades.py
+++ losses/trades.py
@@ -46,8 +46,8 @@ def trades_loss(args,
     optimizer.zero_grad()
     # logits_nat = model(x_natural)
     # logits_adv = model(x_adv)
-    logits_nat, logits_adv = model.custom_forward(x_natural, x_adv)
-    
+    logits_nat, logits_adv = model(x_natural, x_adv)
+
     clean_values = F.cross_entropy(logits_nat, y, reduction='none')
     robust_values = nn.KLDivLoss(reduction='none')( F.log_softmax(logits_adv, dim=1), F.softmax(logits_nat, dim=1) ).sum(dim=1)   
     loss_values = clean_values + args.beta * robust_values
