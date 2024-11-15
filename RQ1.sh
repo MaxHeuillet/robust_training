9ba8f0ab61d60cbb781168b0ebcdb74fc9f08bc3
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-11-14 22:10:30 -0500

    update

--- RQ1.sh
+++ RQ1.sh
@@ -3,9 +3,9 @@
 # Define variables
 seeds=1
 #archs=( 'convnext' ) # 'resnet50' 'vitsmall'
-datas=(  'Flowers', 'CIFAR10', ) # 'CIFAR100', 'EuroSAT', 'Aircraft' 
+datas=(  'Flowers', 'CIFAR10' ) # 'CIFAR100', 'EuroSAT', 'Aircraft' 
 task='train'
-losses=( 'TRADES_v2' , ) # 'APGD' 
+losses=( 'TRADES_v2'  ) # 'APGD' 
 sched='nosched'
 iterations=2 #50
 aug='aug'
@@ -20,20 +20,20 @@ backbones=( 'convnext_base',  'convnext_base.fb_in22k', 'robust_convnext_base',
             'robust_wideresnet_28_10',  'deit_small_patch16_224.fb_in1k','robust_deit_small_patch16_224',
             'vit_base_patch16_224.augreg_in1k', 'vit_base_patch16_224.augreg_in21k', 'robust_vit_base_patch16_224'  ) 
 
-ft_type=( 'full_fine_tuning', ) #'lora' ,  'linear_probing'
+ft_type=( 'full_fine_tuning' ) #'lora' ,  'linear_probing'
 
 # Loop over architectures, pruning ratios, strategies, and learning rates
 for data in "${datas[@]}"; do
   for loss in "${losses[@]}"; do
-      for pruning_ratio in "${pruning_ratios[@]}"; do
-        for pruning_strategy in "${pruning_strategies[@]}"; do
-          for batch_strategy in "${batch_strategies[@]}"; do
-            for init_lr in "${init_lrs[@]}"; do
-              for id in $(seq 1 $seeds); do
-                for bckbn in "${backbones[@]}"; do
-                  for fttype in "${ft_type[@]}"; do
+    for pruning_ratio in "${pruning_ratios[@]}"; do
+      for pruning_strategy in "${pruning_strategies[@]}"; do
+        for batch_strategy in "${batch_strategies[@]}"; do
+          for init_lr in "${init_lrs[@]}"; do
+            for id in $(seq 1 $seeds); do
+              for bckbn in "${backbones[@]}"; do
+                for fttype in "${ft_type[@]}"; do
                     
-                    if [ "$CC_CLUSTER" = "beluga" ]; then
+                  if [ "$CC_CLUSTER" = "beluga" ]; then
           
               ### Pretrained non robust
               sbatch --export=ALL,\
