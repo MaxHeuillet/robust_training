36110f6aa0b7d7fb7faea6b72cb1d324a90b12a3
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-11-17 17:45:44 -0500

    update

--- utils/setup.py
+++ utils/setup.py
@@ -43,15 +43,15 @@ class Setup:
 
         # Batch size recommendations based on the backbone
         if self.args.backbone == 'robust_wideresnet_28_10':
-            batch_size = 8 * base  # WideResNet-28-10 is memory-intensive due to wide layers
+            batch_size = 16 * base  # 8 = OK, 
         elif self.args.backbone in ['deit_small_patch16_224.fb_in1k', 'robust_deit_small_patch16_224']:
-            batch_size = 16 * base  # DeiT Small models are efficient
+            batch_size = 32 * base  # 16 = OK, 
         elif self.args.backbone in ['vit_base_patch16_224.augreg_in1k', 'vit_base_patch16_224.augreg_in21k', 'robust_vit_base_patch16_224']:
-            batch_size = 16 * base  # ViT Base models have higher memory usage
+            batch_size = 32 * base  # 16 = OK, 
         elif self.args.backbone in ['convnext_base', 'convnext_base.fb_in22k', 'robust_convnext_base']:
-            batch_size = 16 * base  # ConvNeXt Base models are sizable
+            batch_size = 32 * base  # 16 = OK,
         elif self.args.backbone in ['convnext_tiny', 'convnext_tiny.fb_in22k', 'robust_convnext_tiny']:
-            batch_size = 16 * base  # ConvNeXt Tiny models are lighter
+            batch_size = 32 * base  # 16 = OK,
 
         return batch_size
         
