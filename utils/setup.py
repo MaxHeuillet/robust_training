5951846d6f84191ff3c8525ff5c2b779f57f325b
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-11-17 18:04:18 -0500

    update

--- utils/setup.py
+++ utils/setup.py
@@ -34,6 +34,7 @@ class Setup:
 
         
     def train_batch_size(self):
+
         cluster_name = os.environ.get('SLURM_CLUSTER_NAME', 'Unknown')
         
         if cluster_name == 'narval':
@@ -45,7 +46,7 @@ class Setup:
         if self.args.backbone == 'robust_wideresnet_28_10':
             batch_size = 8 * base  # 8 = OK, 16 = NOT OK, 12 NOT OK
         elif self.args.backbone in ['deit_small_patch16_224.fb_in1k', 'robust_deit_small_patch16_224']:
-            batch_size = 256 * base  # 16 = OK, 32 = OK, 64 = OK, 128 = OK
+            batch_size = 128 * base  # 16 = OK, 32 = OK, 64 = OK, 128 = OK, 256 = NOT OK
         elif self.args.backbone in ['vit_base_patch16_224.augreg_in1k', 'vit_base_patch16_224.augreg_in21k', 'robust_vit_base_patch16_224']:
             batch_size = 64 * base  # 16 = OK, 32 = OK, 64 = OK, 128 = NOT OK
         elif self.args.backbone in ['convnext_base', 'convnext_base.fb_in22k', 'robust_convnext_base']:
@@ -57,12 +58,24 @@ class Setup:
         
     def test_batch_size(self,):
 
-        if os.environ.get('SLURM_CLUSTER_NAME', 'Unknown') == 'narval' and self.args.backbone == 'robust_wideresnet_28_10':
-            batch_size = 8
-        elif os.environ.get('SLURM_CLUSTER_NAME', 'Unknown') == 'narval' and self.args.backbone != 'robust_wideresnet_28_10':
-            batch_size = 64
-        else:
-            batch_size = 16
+        cluster_name = os.environ.get('SLURM_CLUSTER_NAME', 'Unknown')
+        
+        if cluster_name == 'narval':
+            base = 2.5
+        elif cluster_name == 'beluga':
+            base = 1
+
+        # Batch size recommendations based on the backbone
+        if self.args.backbone == 'robust_wideresnet_28_10':
+            batch_size = 8 * base  # 8 = OK, 16 = NOT OK, 12 NOT OK
+        elif self.args.backbone in ['deit_small_patch16_224.fb_in1k', 'robust_deit_small_patch16_224']:
+            batch_size = 128 * base  # 16 = OK, 32 = OK, 64 = OK, 128 = OK, 256 = NOT OK
+        elif self.args.backbone in ['vit_base_patch16_224.augreg_in1k', 'vit_base_patch16_224.augreg_in21k', 'robust_vit_base_patch16_224']:
+            batch_size = 64 * base  # 16 = OK, 32 = OK, 64 = OK, 128 = NOT OK
+        elif self.args.backbone in ['convnext_base', 'convnext_base.fb_in22k', 'robust_convnext_base']:
+            batch_size = 32 * base  # 16 = OK, 32 = OK, 64 = NOT OK
+        elif self.args.backbone in ['convnext_tiny', 'convnext_tiny.fb_in22k', 'robust_convnext_tiny']:
+            batch_size = 64 * base  # 16 = OK, 32 = OK, 64 = OK, 128 = NOT OK
         
         # if os.environ.get('SLURM_CLUSTER_NAME', 'Unknown') == 'beluga' and self.args.backbone == 'robust_wideresnet_28_10':
         #     batch_size = 16
