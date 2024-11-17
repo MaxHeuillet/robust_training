dd78863e4af29f2ee8a608e408d4528949148bc8
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-11-16 23:16:16 -0500

    update

--- distributed_experiment1.py
+++ distributed_experiment1.py
@@ -174,7 +174,7 @@ class BaseExperiment:
 
         trainloader, valloader, train_sampler, val_sampler = self.initialize_loaders(rank)
 
-        print('N', self.args.N, flush=True)
+        # print('N', self.args.N, flush=True)
 
         model = load_architecture(self.args)
         model = CustomModel(self.args, model)
