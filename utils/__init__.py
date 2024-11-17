aac83c0dc88ee833ede2f972293c35248965eefc
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-11-17 17:04:28 -0500

    update

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,6 +5,7 @@ from .seed_settings import set_seeds
 from .exp_decay import FitExpDecay
 from .exp_decay_v2 import FitExpDecay_v2
 from .exp_decay_v3 import FitExpDecay_v3
+from .dormant_neurons import ActivationTracker, register_hooks, compute_stats
 # from .auto_naming import get_exp_name
 # from .gradients import Adahessian
 # from .submodular import *
