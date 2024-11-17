b088b85035cda34a253a9ef47ea86e00003c3071
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-11-17 17:50:22 -0500

    update

--- utils/setup.py
+++ utils/setup.py
@@ -43,15 +43,15 @@ class Setup:
 
         # Batch size recommendations based on the backbone
         if self.args.backbone == 'robust_wideresnet_28_10':
-            batch_size = 16 * base  # 8 = OK, 
+            batch_size = 12 * base  # 8 = OK, 16 = NOT OK,
         elif self.args.backbone in ['deit_small_patch16_224.fb_in1k', 'robust_deit_small_patch16_224']:
-            batch_size = 32 * base  # 16 = OK, 
+            batch_size = 64 * base  # 16 = OK, 32 = OK
         elif self.args.backbone in ['vit_base_patch16_224.augreg_in1k', 'vit_base_patch16_224.augreg_in21k', 'robust_vit_base_patch16_224']:
-            batch_size = 32 * base  # 16 = OK, 
+            batch_size = 64 * base  # 16 = OK, 32 = OK
         elif self.args.backbone in ['convnext_base', 'convnext_base.fb_in22k', 'robust_convnext_base']:
-            batch_size = 32 * base  # 16 = OK,
+            batch_size = 64 * base  # 16 = OK, 32 = OK
         elif self.args.backbone in ['convnext_tiny', 'convnext_tiny.fb_in22k', 'robust_convnext_tiny']:
-            batch_size = 32 * base  # 16 = OK,
+            batch_size = 64 * base  # 16 = OK, 32 = OK
 
         return batch_size
         
