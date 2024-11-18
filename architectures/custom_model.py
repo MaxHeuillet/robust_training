32f9757cfc0c5582fb0c3b3e47f653883e349990
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-11-18 11:31:34 -0500

    update

--- architectures/custom_model.py
+++ architectures/custom_model.py
@@ -16,24 +16,38 @@ class CustomModel(nn.Module):
         else:
             return logits_1
 
-    def set_fine_tuning_strategy(self, strategy):
-        if strategy == 'full_fine_tuning':
-            self._enable_all_gradients()
-        elif strategy == 'linear_probing':
+    def set_fine_tuning_strategy(self, ):
+
+        if self.args.strategy == 'full_fine_tuning':
             self._freeze_backbone()
             self._unfreeze_last_layer()
-        elif strategy == 'lora':
+
+        elif self.args.strategy == 'linear_probing':
             self._freeze_backbone()
-            self._apply_lora_adapters()
+            self._unfreeze_last_layer()
+
+        # elif self.args.strategy == 'lora':
+        #     self._freeze_backbone()
+        #     self._apply_lora_adapters()
+
         else:
-            # TODO: hybrid approaches
-            raise ValueError(f"Unknown fine-tuning strategy: {strategy}")
+            raise ValueError(f"Unknown fine-tuning strategy: {self.args.strategy}")
+        
+    def update_fine_tuning_strategy(self, iteration):
+
+        if self.args.strategy == 'full_fine_tuning' and iteration >= self.args.freeze_epochs and self.all_gradients_on==False:
+            print('Unfreezing all layers')
+            self._enable_all_gradients()
+            
+
 
     def _enable_all_gradients(self):
+        self.all_gradients_on = True
         for param in self.base_model.parameters():
             param.requires_grad = True
 
     def _freeze_backbone(self):
+        self.all_gradients_on = False
         # Freeze all parameters in the model
         for param in self.base_model.parameters():
             param.requires_grad = False
@@ -44,45 +58,45 @@ class CustomModel(nn.Module):
         for param in last_layer.parameters():
             param.requires_grad = True
 
-    def _apply_lora_adapters(self):
+    # def _apply_lora_adapters(self):
 
-        if 'convnext' in self.args.backbone:
+    #     if 'convnext' in self.args.backbone:
 
-            # Configure LoRA to target intermediate Conv2d and Linear layers only
-            lora_config = LoraConfig(
-                r=8,  # Rank of the low-rank adapters
-                lora_alpha=32,  # Scaling factor for the LoRA adapters
-                target_modules=["conv_dw", "mlp.fc1", "mlp.fc2"],  # Apply LoRA to these layers across the intermediate blocks
-                lora_dropout=0.1,  # Dropout rate for LoRA adapters
-                bias="none"  # No bias term for the adapters
-            )
+    #         # Configure LoRA to target intermediate Conv2d and Linear layers only
+    #         lora_config = LoraConfig(
+    #             r=8,  # Rank of the low-rank adapters
+    #             lora_alpha=32,  # Scaling factor for the LoRA adapters
+    #             target_modules=["conv_dw", "mlp.fc1", "mlp.fc2"],  # Apply LoRA to these layers across the intermediate blocks
+    #             lora_dropout=0.1,  # Dropout rate for LoRA adapters
+    #             bias="none"  # No bias term for the adapters
+    #         )
 
-            self.base_model = get_peft_model(self.base_model, lora_config)
+    #         self.base_model = get_peft_model(self.base_model, lora_config)
 
-        elif 'wideresnet' in self.args.backbone:
+    #     elif 'wideresnet' in self.args.backbone:
 
-            # LoRA configuration to apply adapters to intermediate Conv2d layers in WideResNet
-            lora_config = LoraConfig(
-                r=8,  # Rank of the low-rank adapters, lower for CNNs than for Transformers
-                lora_alpha=16,  # Scaling factor for LoRA, less aggressive for convolutional layers
-                target_modules=["conv_0", "conv_1"],  # Target only intermediate Conv2d layers
-                lora_dropout=0.1,  # Regularization to prevent overfitting
-                bias="none"  # No additional bias in LoRA adapters
-            )
+    #         # LoRA configuration to apply adapters to intermediate Conv2d layers in WideResNet
+    #         lora_config = LoraConfig(
+    #             r=8,  # Rank of the low-rank adapters, lower for CNNs than for Transformers
+    #             lora_alpha=16,  # Scaling factor for LoRA, less aggressive for convolutional layers
+    #             target_modules=["conv_0", "conv_1"],  # Target only intermediate Conv2d layers
+    #             lora_dropout=0.1,  # Regularization to prevent overfitting
+    #             bias="none"  # No additional bias in LoRA adapters
+    #         )
 
-            self.base_model = get_peft_model(self.base_model, lora_config)
+    #         self.base_model = get_peft_model(self.base_model, lora_config)
 
-        elif 'vit' in self.args.backbone or 'deit' in self.args.backbone:
+    #     elif 'vit' in self.args.backbone or 'deit' in self.args.backbone:
 
-            lora_config = LoraConfig(
-                r=8,  # Rank of the low-rank adapters, often suitable for transformer blocks
-                lora_alpha=32,  # Scaling factor for the LoRA adapters
-                target_modules=["attn.qkv", "attn.proj", "mlp.fc1", "mlp.fc2"],  # Target only these intermediate layers
-                lora_dropout=0.1,  # Dropout to regularize the adapters
-                bias="none"  # No bias for LoRA adapters
-                )
+    #         lora_config = LoraConfig(
+    #             r=8,  # Rank of the low-rank adapters, often suitable for transformer blocks
+    #             lora_alpha=32,  # Scaling factor for the LoRA adapters
+    #             target_modules=["attn.qkv", "attn.proj", "mlp.fc1", "mlp.fc2"],  # Target only these intermediate layers
+    #             lora_dropout=0.1,  # Dropout to regularize the adapters
+    #             bias="none"  # No bias for LoRA adapters
+    #             )
             
-            self.base_model = get_peft_model(self.base_model, lora_config)
+    #         self.base_model = get_peft_model(self.base_model, lora_config)
 
 
 
