65a8f46f6c69c01a2a9ccdecc5328505eb4f07cb
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-11-25 11:35:37 -0500

    update

--- utils/optimizer_loader.py
+++ utils/optimizer_loader.py
@@ -1,35 +1,32 @@
 from torch.optim import AdamW
 
-
-
-def load_optimizer(args, model, ):
-
+def load_optimizer(args, model):
     if "convnext" in args.backbone:
-
-        optimizer = AdamW( [
-            {'params': model.parameters(), 'lr': args.init_lr, 'weigh_decay':args.weight_decay,} ,
-            {'params': model.head.fc.parameters(), 'lr': 0.001, 'weigh_decay':args.weight_decay,},  
-                    ])
-
+        optimizer = AdamW([
+            {'params': [p for n, p in model.named_parameters() if "head.fc" not in n], 
+             'lr': args.init_lr, 'weight_decay': args.weight_decay},
+            {'params': model.head.fc.parameters(), 
+             'lr': 0.001, 'weight_decay': args.weight_decay},
+        ])
     elif "wideresnet" in args.backbone:
-
-        optimizer = AdamW( [
-            {'params': model.parameters(), 'lr': args.init_lr, 'weigh_decay':args.weight_decay,}, 
-            {'params': model.logits.parameters(), 'lr': 0.001, 'weigh_decay':args.weight_decay,},  
-                    ])
-
+        optimizer = AdamW([
+            {'params': [p for n, p in model.named_parameters() if "logits" not in n], 
+             'lr': args.init_lr, 'weight_decay': args.weight_decay},
+            {'params': model.logits.parameters(), 
+             'lr': 0.001, 'weight_decay': args.weight_decay},
+        ])
     elif "deit" in args.backbone:
-
-        optimizer = AdamW( [
-            {'params': model.parameters(), 'lr': args.init_lr, 'weigh_decay':args.weight_decay,} ,
-            {'params': model.head.parameters(), 'lr': 0.001, 'weigh_decay':args.weight_decay,}, 
-                    ])
-
+        optimizer = AdamW([
+            {'params': [p for n, p in model.named_parameters() if "head" not in n], 
+             'lr': args.init_lr, 'weight_decay': args.weight_decay},
+            {'params': model.head.parameters(), 
+             'lr': 0.001, 'weight_decay': args.weight_decay},
+        ])
     elif "vit" in args.backbone:
-
-        optimizer = AdamW( [
-            {'params': model.parameters(), 'lr': args.init_lr, 'weigh_decay':args.weight_decay,},
-            {'params': model.head.parameters(), 'lr': 0.001, 'weigh_decay':args.weight_decay,}, 
-                    ])
-
+        optimizer = AdamW([
+            {'params': [p for n, p in model.named_parameters() if "head" not in n], 
+             'lr': args.init_lr, 'weight_decay': args.weight_decay},
+            {'params': model.head.parameters(), 
+             'lr': 0.001, 'weight_decay': args.weight_decay},
+        ])
     return optimizer
