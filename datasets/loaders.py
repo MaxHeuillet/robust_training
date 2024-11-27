733d5f67b45363e4945579f41de539f15426e5d5
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-11-27 16:23:48 -0500

    update

--- datasets/loaders.py
+++ datasets/loaders.py
@@ -30,7 +30,7 @@ def load_data(args):
     transform = transforms.Compose([transforms.Resize((224, 224)),  # Resize images to 224x224
                                     transforms.ToTensor(),
                                     transforms.Normalize( mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225] ),])
-
+        
     if args.dataset == 'CIFAR10':
         
         N = 10
