2c81171fbfb29fc83d17b7046c635c774971d935
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-11-27 18:27:56 -0500

    update

--- utils/arguments.py
+++ utils/arguments.py
@@ -4,23 +4,14 @@ import os
 
 
 def get_exp_name(args):
+    # Access all attributes of args as a dictionary
+    args_dict = vars(args) if hasattr(args, '__dict__') else args.__dict__
 
-    grd = ""
-    grd += args.loss_function
-    grd += f"_{args.init_lr}"
-    grd += f"_{args.sched}"
-    grd += f"_{args.dataset}" 
-    grd += f"_{args.backbone}" 
+    # Create the experiment name dynamically by concatenating all key-value pairs
+    exp_name = "_".join(f"{key}={value}" for key, value in args_dict.items())
 
-    grd += f"_{args.pruning_strategy}" 
-    grd += f"_{args.pruning_ratio}"  
+    return exp_name
 
-    grd += f"_{args.batch_strategy}"          
-    grd += f"_{args.iterations}"
-    
-    grd += f"_{args.seed}"
-
-    return grd
 
 def get_args():
 
@@ -77,6 +68,58 @@ def get_args():
 
     ### arguments for diffusion augmented learning:
     parser.add_argument("--unsup_fraction", default=0.3, type=float, help="fraction of data generated from diffusion model")
+    parser.add_argument("--exp", default='', type=str, help="the experiment type")
+    
+    args, unknown = parser.parse_known_args()
+
+    #args.device = "cuda" if torch.cuda.is_available() else "cpu"
+    
+    return args
+
+
+def get_args2():
+
+    parser = argparse.ArgumentParser(allow_abbrev=False)
+
+    if "calculquebec" in os.uname().nodename:  # Check for a substring that is unique to the cluster
+        default_data_dir = '~/scratch/data'
+    elif "calcul.quebec" in os.uname().nodename:
+        default_data_dir = '~/scratch/data'
+    else:
+        default_data_dir = './data'
+
+    # General options
+    # parser.add_argument("--arch", default="resnet50",type=str, help="model architecture")
+    # parser.add_argument("--pre_trained", type=str, help="load pretrained non robust model")
+    parser.add_argument("--backbone", default = 'none', type=str, help="load backbone")
+    parser.add_argument("--ft_type", default = 'none', type=str, help="fine tuning type")
+    parser.add_argument("--freeze_epochs", default=10, type=int, help="number of preliminary epochs where we only do linear probing")
+
+    parser.add_argument('--dataset', default='CIFAR10', type=str, help='dataset: ' + ' (default: cifar10)')
+    
+    parser.add_argument("--iterations", default=10, type=int, metavar="N", help="number of total iterations to run")
+
+    parser.add_argument("--loss_function", default="TRADES_v2", type=str, help="the loss function")
+    
+    parser.add_argument('--seed', default=0, type=int, help="random seed")
+
+    parser.add_argument("--log_dir", default="./logs", type=str, help="The directory used to save logs")
+    parser.add_argument("--statedict_dir", default="./state_dicts", type=str, help="The directory used to save state dics")
+    parser.add_argument('--data_dir', default=default_data_dir,type=str,)
+    print(default_data_dir)
+
+    parser.add_argument('--task', default='train',type=str, choices=['train', 'eval'], help="wether to train or to evaluate the model")
+
+    parser.add_argument("--num_workers",default=1, type=int, help="number of data loading workers (default: 4)")
+
+    ### arguments for TRADES loss function:
+    parser.add_argument("--epsilon", default=4/255, type=float, help="epsilon of trades") #8/255
+    parser.add_argument("--step_size", default=2/255, type=float, help="step size of trades")
+    parser.add_argument("--perturb_steps", default=10, type=int, help="number of steps of trades")
+    parser.add_argument("--beta", default=1.0, type=float, help="beta of trades")
+    parser.add_argument("--distance", default='Linf', type=str, help="distance of trades")
+
+    ### arguments for diffusion augmented learning:
     parser.add_argument("--exp", default='', type=str, help="the experiment type")
     
     args, unknown = parser.parse_known_args()
