baaaaba46d24a2cae9d33c58231bdccf08ec7cb5
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-12-02 10:22:45 -0500

    update

--- RQ3.sh
+++ RQ3.sh
@@ -2,14 +2,14 @@
 
 # Define variables
 seeds=1
-datas=( 'Flowers' ) # 'CIFAR100' 'EuroSAT' 'Aircraft' 'CIFAR10'  
-losses=( 'CLASSIC_AT' ) #'TRADES_v2' 
+datas=( 'Flowers' 'CIFAR100' 'EuroSAT' 'Aircraft' 'CIFAR10'  ) #  
+losses=( 'CLASSIC_AT' 'TRADES_v2' ) #
 
 backbones=( 
             #'deit_small_patch16_224.fb_in1k' 'robust_deit_small_patch16_224' 'random_deit_small_patch16_224' 
             #'vit_base_patch16_224.augreg_in1k' 'vit_base_patch16_224.augreg_in21k', 'robust_vit_base_patch16_224' 'random_vit_base_patch16_224'
             #'convnext_base' 'convnext_base.fb_in22k' 'robust_convnext_base' 'random_convnext_base'
-            'convnext_tiny' # 'robust_convnext_tiny' 'convnext_tiny.fb_in22k' 'random_convnext_tiny' 
+            'convnext_tiny' 'robust_convnext_tiny' 'convnext_tiny.fb_in22k' 'random_convnext_tiny' 
            ) # 'wideresnet_28_10' 'robust_wideresnet_28_10' 
 
 ft_type=( 'full_fine_tuning' ) #'lora' ,  'linear_probing'
