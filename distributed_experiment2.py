0ec342417a6d580a73a59e94c942919e84bd9fa8
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2024-12-03 15:34:37 -0500

    update

--- distributed_experiment2.py
+++ distributed_experiment2.py
@@ -79,7 +79,7 @@ class BaseExperiment:
     def initialize_logger(self, rank):
 
         logger = Experiment(api_key="I5AiXfuD0TVuSz5UOtujrUM9i",
-                                project_name="robust_training_{}_2".format(self.setup.cluster_name),
+                                project_name="robust_training_{}_3".format(self.setup.cluster_name),
                                 workspace="maxheuillet",
                                 auto_metric_logging=False,
                                 auto_output_logging=False)
