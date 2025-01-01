82b2aa139f36393107b7dc009871b82e35ac91db
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2025-01-01 12:58:55 -0500

    update

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,8 +3,8 @@ from .setup import Setup
 from .arguments import get_args2, get_exp_name
 from .seed_settings import set_seeds
 from .exp_decay import FitExpDecay
-from .exp_decay_v2 import FitExpDecay_v2
-from .exp_decay_v3 import FitExpDecay_v3
+# from .exp_decay_v2 import FitExpDecay_v2
+# from .exp_decay_v3 import FitExpDecay_v3
 from .dormant_neurons import ActivationTrackerAggregated, register_hooks_aggregated, compute_stats_aggregated
 from .optimizer_loader import load_optimizer
 from .hp_opt import Hp_opt
