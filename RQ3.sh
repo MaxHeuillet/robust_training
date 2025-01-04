644b45569bb65f32bde4bf046f601b1d73e49123
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2025-01-03 20:31:46 -0500

    update

--- RQ3.sh
+++ RQ3.sh
@@ -3,9 +3,9 @@
 # Define variables
 seeds=1
 
-datas=( 'stanford_cars' 'caltech101' 
+datas=( 'caltech101' 
         'dtd' 'fgvc-aircraft-2013b' 
-        'flowers-102' 'oxford-iiit-pet' ) #'imagenette2' 'eurosat' 
+        'flowers-102' 'oxford-iiit-pet' 'stanford_cars' ) #'imagenette2' 'eurosat' 
 
 losses=(  'TRADES_v2' 'CLASSIC_AT' ) 
 
