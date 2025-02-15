6a19e462681c6237a47ae226c33f5eab1ed4a6ca
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2025-02-15 17:32:48 -0500

    update

--- architectures/__init__.py
+++ architectures/__init__.py
@@ -5,4 +5,4 @@ from .loaders import load_architecture, change_head #, load_statedict
 from .lora import add_lora, set_lora_gradients
 # from .wideresnetswish import wideresnet
 from .custom_model import CustomModel
-from ..old_files.subspaces import SubspaceLinear, SubspaceConv2d
+# from ..old_files.subspaces import SubspaceLinear, SubspaceConv2d
