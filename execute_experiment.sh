49f21b8161de4089c3f207e755205faa132d65ff
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2025-02-17 10:17:45 -0500

    update

--- execute_experiment.sh
+++ execute_experiment.sh
@@ -3,24 +3,22 @@
 # Define variables
 seeds=1
 
-datas=( 'caltech101'  ) 
-        #'uc-merced-land-use-dataset' 'kvasir-dataset' 
-        #  'stanford_cars' 'flowers-102'
+datas=( 'uc-merced-land-use-dataset' 'kvasir-dataset'  ) 
+        #  'stanford_cars' 'flowers-102' 'caltech101'
         #  'oxford-iiit-pet'  'dtd'  'fgvc-aircraft-2013b' 'oxford-iiit-pet' 
 
-losses=( 'CLASSIC_AT' )  #  'TRADES_v2' 
+losses=( 'CLASSIC_AT' 'TRADES_v2'  )  #   
 
-backbones=( 'vit_base_patch16_224.orig_in21k' 'vit_base_patch16_224_miil.in21k' )
+backbones=(
+  'convnext_tiny' 'robust_convnext_tiny' 'convnext_tiny.fb_in22k' 
+  'deit_small_patch16_224.fb_in1k' 'robust_deit_small_patch16_224'
+  'convnext_base' 'convnext_base.fb_in22k' 'robust_convnext_base' 
+  'convnext_base.clip_laion2b' 'convnext_base.clip_laion2b_augreg'
+  'vit_base_patch16_224.augreg_in1k' 'vit_base_patch16_224.augreg_in21k'
+  'vit_base_patch16_224.dino' 'vit_base_patch16_224.mae' 'vit_base_patch16_224.orig_in21k',
+  'vit_base_patch16_224.sam_in1k' 'vit_base_patch16_224_miil.in21k' 'robust_vit_base_patch16_224' ) 
 
-#'convnext_tiny' 'robust_convnext_tiny' 'convnext_tiny.fb_in22k' 
-#    'robust_convnext_base' 'vit_base_patch16_224.augreg_in1k'
 
-#'deit_small_patch16_224.fb_in1k' 'robust_deit_small_patch16_224'
-#   'convnext_base' 'convnext_base.fb_in22k'
-#   'convnext_base.clip_laion2b' 'convnext_base.clip_laion2b_augreg'
-#   'vit_base_patch16_224.augreg_in1k' 'vit_base_patch16_224.augreg_in21k'
-#  'vit_base_patch16_224.dino' 'vit_base_patch16_224.mae' 
-#  'vit_base_patch16_224.sam_in1k'  'robust_vit_base_patch16_224'
 
 
 # Get the project name 
