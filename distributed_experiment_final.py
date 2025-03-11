5fe27806c7234a4f69673148ee41e915c0ea847b
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2025-03-11 15:48:10 -0400

    uncomment the break statements for easier debugging

--- distributed_experiment_final.py
+++ distributed_experiment_final.py
@@ -431,7 +431,7 @@ class BaseExperiment:
                 nb_correct_adv += (preds_adv == target).sum().item()
                 nb_examples += target.size(0)
 
-                # break
+                break
 
             stats_nat = { 'nb_correct':nb_correct_nat, 'nb_examples':nb_examples }
             stats_adv = { 'nb_correct':nb_correct_adv, 'nb_examples':nb_examples }
@@ -459,7 +459,7 @@ class BaseExperiment:
                 nb_correct_adv += (preds_adv == target).sum().item()
                 nb_examples += target.size(0)
 
-                # break
+                break
             
             stats_nat = { 'nb_correct':None, 'nb_examples':None }
             stats_adv = { 'nb_correct':nb_correct_adv, 'nb_examples':nb_examples }
