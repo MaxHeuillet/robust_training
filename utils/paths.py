17d0d0178f94b2c28509e33d30bf9cc81db28c89
Maxime Heuillet <mheuill@beluga3.int.ets1.calculquebec.ca>
2025-03-13 14:38:08 -0400

    update paths

--- utils/paths.py
+++ utils/paths.py
@@ -30,10 +30,10 @@ def get_state_dict_dir(hp_opt,config):
 
     if "calculquebec" in os.uname().nodename or "calcul.quebec" in os.uname().nodename:  # Check for a substring that is unique to the cluster
         # this is to load state dict (not during HP opt), you can specify relative path to your state dict directory
-        statedict_dir = './state_dicts/' #TO UPDATE
+        statedict_dir = '~/scratch/state_dicts_share/' #TO UPDATE
         if hp_opt:
             ### this is to load state dict during HP OPT, you must specify an absolute path to the directory
-            statedict_dir = "/home/mheuill/projects/def-adurand/mheuill/robust_training/state_dicts/" #TOUP
+            statedict_dir = "/home/mheuill/scratch/state_dicts_share/" #TOUP
     else:
         # this is if you run prototypes locally
         statedict_dir = "/home/mheuillet/Desktop/robust_training/state_dicts/"
