c9a4147056dc6adbf36eef3f67942242677a010b
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2025-03-14 18:10:04 -0400

    update

--- distributed_experiment_final.sh
+++ distributed_experiment_final.sh
@@ -18,47 +18,7 @@ source ~/scratch/MYENV4/bin/activate
 
 export PYTHONUNBUFFERED=1
 
-echo "Processing dataset: ${DATA}"
-
-# Create a temporary data directory
-mkdir -p $SLURM_TMPDIR/data
-
-# Define the path to the dataset archive (without extension)
-# this is the directory with all the dataset archives
-ARCHIVE_PATH=~/scratch/data/${DATA}
-
-# Function to extract .tar.zst archives
-extract_tar_zst() {
-    local archive="${ARCHIVE_PATH}.tar.zst"
-    echo "Extracting ${archive} with tar + zstd..."
-    tar -I zstd -xf "$archive" -C "$SLURM_TMPDIR/data"
-}
-
-# Function to extract .zip archives
-extract_zip() {
-    local archive="${ARCHIVE_PATH}.zip"
-    echo "Extracting ${archive} with unzip..."
-    unzip -q "$archive" -d "$SLURM_TMPDIR/data"
-}
-
-# If your dataset is zipped, add its name to the OR list below.
-if [ "$DATA" == "stanford_cars" ] || \
-   [ "$DATA" == "uc-merced-land-use-dataset" ] || \
-   [ "$DATA" == "kvasir-dataset" ]; then
-    echo "Dataset is $DATA. Using unzip to extract."
-    extract_zip
-else
-    echo "Dataset is not in the zip list. Using tar.zst to extract."
-    extract_tar_zst
-fi
-
-# Check if extraction was successful
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to extract ${DATA} archive." >&2
-    exit 1
-fi
-
-echo "Extraction of ${DATA} archive completed successfully at $(date)."
+bash ./dataset_to_tmdir.sh "$DATA"
 
 # Run the Python experiment script with appropriate arguments
 python ./distributed_experiment_final.py \
