e7ea1d9632d0e3cf9c03a5652bcbd7c7d601fb01
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2025-04-07 16:58:45 -0400

    update

--- utils/setup.py
+++ utils/setup.py
@@ -102,11 +102,11 @@ class Setup:
         elif any(x in arch_lower for x in ['convnext_base', ]):
             base_bs = 20
         elif any(x in arch_lower for x in ['deit_small', 'eva02_tiny', 'swin_tiny', 'coatnet_0', 'vit_small']):
-            base_bs = 80
+            base_bs = 75
         elif any(x in arch_lower for x in ['vit_base', 'swin_base', 'eva02_base', ]):
             base_bs = 35
         elif 'resnet50' in arch_lower:
-            base_bs = 60
+            base_bs = 55
         else:
             print(f"WARNING: unrecognized backbone '{config.backbone}', using fallback base_bs={base_bs}.")
 
