ad59709d4ef31405dfd3f29b82046c5451001b78
Maxime Heuillet <maxime.heuillet.france@gmail.com>
2025-04-10 16:34:48 -0400

    update

---
+++ execute_setup.sh
@@ -0,0 +1,9 @@
+
+module --force purge
+module load StdEnv/2023 gcc/12.3 cuda/12.2 opencv/4.9.0 python/3.11 arrow/18.1.0 scipy-stack/2024a nccl/2.18.3 httpproxy
+
+python3.11 -m venv $SLURM_TMPDIR/myenv_reprod
+source $SLURM_TMPDIR/myenv_reprod/bin/activate
+pip install -r ~/projects/def-adurand/mheuill/robust_training/requirements.txt
+
+export PYTHONUNBUFFERED=1
--- job3_test_linf.sh
+++ job3_test_linf.sh
@@ -7,15 +7,7 @@
 #SBATCH --time=02:58:55
 #SBATCH --mail-type=ALL
 
-# Purge all loaded modules and load necessary ones
-module --force purge
-module load StdEnv/2023 gcc/12.3 cuda/12.2 opencv/4.9.0 python/3.11 arrow/18.1.0 scipy-stack/2024a nccl/2.18.3 httpproxy
-
-python3.11 -m venv $SLURM_TMPDIR/myenv_reprod
-source $SLURM_TMPDIR/myenv_reprod/bin/activate
-pip install -r ~/projects/def-adurand/mheuill/robust_training/requirements.txt
-
-export PYTHONUNBUFFERED=1
+bash ./execute_setup.sh
 
 # --- HPO Step ---
 python ./distributed_experiment_final.py \
