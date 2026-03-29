873ba3b66d366de299427e75dc5ff5eef3a7cb51
Maxime Heuillet <maximeheuillet@MacBook-Pro-de-Maxime.local>
2026-03-29 11:08:14 -0400

    update

--- execute_setup.sh
+++ execute_setup.sh
@@ -6,5 +6,8 @@ python3.11 -m venv $SLURM_TMPDIR/myenv_reprod
 source $SLURM_TMPDIR/myenv_reprod/bin/activate
 pip install -r ./requirements.txt
 
+pip install "torch>=2.4" "torchvision>=0.20" --upgrade
+pip install "transformers>=4.49" "tokenizers>=0.21" "safetensors>=0.4.3" --upgrade
+
 export PYTHONUNBUFFERED=1
 
