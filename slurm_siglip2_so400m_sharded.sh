443890df96b0cb83f5b8ebe41098f734fc64b736
Maxime Heuillet <maximeheuillet@MacBook-Pro-de-Maxime.local>
2026-04-07 11:15:51 -0300

    update

--- slurm_siglip2_so400m_sharded.sh
+++ slurm_siglip2_so400m_sharded.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=48
 #SBATCH --gres=gpu:h100:4
 #SBATCH --mem=480G
-#SBATCH --time=11:59:00
+#SBATCH --time=02:59:00
 #SBATCH --mail-type=ALL
 #SBATCH --output=logs/metaclip_h14/%x_%j.out
 #SBATCH --error=logs/metaclip_h14/%x_%j.err
